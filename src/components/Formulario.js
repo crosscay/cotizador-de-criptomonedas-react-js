e93c8f0a2557293cbebb8e15ae7e696759fd3255
crosscay <caicedocarlos0306@gmail.com>
2020-08-23 19:35:25 -0500

    validar el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+import Error from './Error';
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
 import axios from 'axios';
@@ -27,6 +28,8 @@ const Formulario = () => {
     // state del listado de criptomonedas
     const [ listacripto, guardarCriptomonedas ] = useState([]);
 
+    const [ error, guardarError ] = useState(false);
+
     const MONEDAS = [
         { codigo: 'USD', nombre: 'Dolar de Estados Unidos' },
         { codigo: 'CLP', nombre: 'Peso Chileno' },
@@ -53,8 +56,27 @@ const Formulario = () => {
         consultarAPI();
     }, []); 
 
+
+    // cuando el usuario hace submit
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        // validar si ambos campos estan llenos
+        if(moneda === '' && criptomoneda === '') {
+            guardarError(true);
+            return;
+        }
+
+        // pasar los datos al componente principal
+        guardarError(false);
+
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            { error ? <Error mensaje="Todos los campos son obligatorios"/>: null }
             <SelectMonedas />
             <SelectCripto />
             <Boton 
