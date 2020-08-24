f58fa1522fdb1f40b27089754a8b91ad191cdb45
crosscay <caicedocarlos0306@gmail.com>
2020-08-23 20:59:27 -0500

    Updates

---
+++ precache-manifest.7f32d4b3444b96314677ef9668436fee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1f7de9b5f31da651e141b4c9761c73",
+    "url": "/cotizador_de_criptomonedas/index.html"
+  },
+  {
+    "revision": "03d5d955df69d13173b1",
+    "url": "/cotizador_de_criptomonedas/static/css/main.b5d55fa2.chunk.css"
+  },
+  {
+    "revision": "77b4358e9d5c26a6988c",
+    "url": "/cotizador_de_criptomonedas/static/js/2.0762855f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cotizador_de_criptomonedas/static/js/2.0762855f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d5d955df69d13173b1",
+    "url": "/cotizador_de_criptomonedas/static/js/main.b198a623.chunk.js"
+  },
+  {
+    "revision": "6897bd3b2c5637fb16aa",
+    "url": "/cotizador_de_criptomonedas/static/js/runtime-main.87bfecb8.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cotizador_de_criptomonedas/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
